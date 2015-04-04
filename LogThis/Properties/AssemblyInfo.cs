7656cfcad106f1e4574123546641747c256bb4ed
Joe Good <joe.d.good@gmail.com>
2015-04-03 23:00:48 -0500

    Changed:  Assembly name from "BabyLog" to "LogThis"

--- LogThis/Properties/AssemblyInfo.cs
+++ LogThis/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BabyLog")]
+[assembly: AssemblyTitle("LogThis")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
